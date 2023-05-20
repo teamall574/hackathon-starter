14037bace054593136dcbdfc2508c68e5a8dd993
teamall574 <100065666+teamall574@users.noreply.github.com>
2023-05-20 20:45:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -32,9 +32,18 @@ resource "null_resource" "execute" {
         "sudo apt-get update -y",
         "sleep 20",
         "sudo curl https://get.docker.com | bash",
-        "docker pull mongo", 
-        "sudo docker run -d -p 27017:27017 -e username=mongoadmin -e mongo_password=password -e mango_database=test  mongo",
-        "docker ps",
+        "sudo apt-get install gnupg",
+        "curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+           sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+            --dearmor"
+        "echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list",
+        "sudo apt-get update",
+        "sudo apt-get install -y mongodb-org",
+        "sudo systemctl start mongod",
+        "mongosh",
+        "use test",
+        "db.createCollection("mycollection")",
+        "db.createUser({ user: "admin", pwd: "admin123", roles: [] })",
         ]
     }
     depends_on = [aws_instance.foo]
