019300113be465f225c587b544a959fd610b54e3
teamall574 <100065666+teamall574@users.noreply.github.com>
2023-05-23 13:07:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -35,7 +35,7 @@ resource "null_resource" "execute" {
         "sudo apt-get install gnupg",
         "curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
            sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-            --dearmor"
+            --dearmor",
         "echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list",
         "sudo apt-get update",
         "sudo apt-get install -y mongodb-org",
